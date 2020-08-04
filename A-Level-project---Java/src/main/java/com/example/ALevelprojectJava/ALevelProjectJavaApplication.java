03465042b16efe7b0c53096e068a65bdeb712569
JBG1233 <james.t.badham.gill@gmail.com>
2020-08-04 14:04:15 +0100

    Add files via upload

---
+++ A-Level-project---Java/src/main/java/com/example/ALevelprojectJava/ALevelProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ALevelprojectJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALevelProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALevelProjectJavaApplication.class, args);
+	}
+
+}
